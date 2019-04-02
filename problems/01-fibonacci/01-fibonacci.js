e18b2b47a74c15c98abfa5fad1a92c32644ea4f9
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-04-02 18:30:19 -0400

    fibonacci finished

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,17 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+let num2 = 1;
+let num1 = 0;
+let num3 = 0;
+for (i = 0; i <= n; i++)
+{  
+  num3 = num1;
+  num1 = num1 + num2;
+  num2 = num3;
+  
 }
+return num1;
+}
+
+console.log(fib(6));
