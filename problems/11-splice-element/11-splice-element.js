2cc390c6881bcdca591a348f58e39e13138879da
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-04-03 19:43:50 -0400

    Splice Element is finished

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,19 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+    let splitarr1 = [];
+    let splitarr2 = [];
+  for(i = 0; i <= index - 1; i++){
+    splitarr1.push(someArr[i]);
+  }
+  for(i = index; i <= someArr.length; i++){
+      splitarr2.push(someArr[i]);
+  }
+  console.log(`First part of element split ${splitarr1}`);
+  console.log(`Second part of element split ${splitarr2}`);
 }
+
+let index = 5;
+let arr = [50, 30, 'Hello', true, 8, 'What', false];
+
+spliceElement(arr, index);
