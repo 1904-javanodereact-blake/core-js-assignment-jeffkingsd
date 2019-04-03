602167487ae1df47f20a0cbb8e4ee9fa8f6587f2
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-04-02 23:30:18 -0400

    Even number is finished

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,25 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+
+   let hold;
+   let isEvennum;
+  hold = someNum / 2;
+    hold = hold - Math.floor(hold);
+    isEvennum = hold * 2
+    if (isEvennum == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+console.log('Set of 10 numbers')
+for (i = 0; i < 10; i++)
+{
+let num = Math.floor(Math.random()*1000+1);
+
+console.log(`Number ${i+1} is ${num} Is num even? ${isEven(num)}`);
 }
