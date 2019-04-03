a52857a9fad703e9f0afd6a9539284eee91931b4
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-04-03 19:32:43 -0400

    Object Creation is finished

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,31 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+class Object1 {
+        constructor(name, age){
+            this.name = name;
+            this.age = age;
+        }
+}
+
+let jeff = new Object1('Jeffrey', 88);
+
+console.log(jeff);
+
+function object2(name, age){
+    this.name = name;
+    this.age =age;
+}
+
+let kevin = new object2('Kevin', 728);
+console.log(kevin);
+
+let object3 = {
+    name: 'Kadi',
+    age: 40
+}
+
+console.log(`Object3
+    name is ${object3.name}
+    age is ${object3.age}`);
