c320b4cf9bb9bb39f83321de3532f28512bf232c
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-04-02 21:38:23 -0400

    Factorial is finished

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -2,5 +2,20 @@
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) {
-  
+    let hold = sumNum;
+    let temp = 0;
+    let count = sumNum;
+    for (i = 0; i < count; i++)
+    {
+        if( sumNum != 1 )
+        {
+            hold =  hold * (sumNum-1);
+            sumNum--;
+        }
+    }
+    return hold;
 }
+
+let num1 = Math.floor(Math.random()*10+1);
+console.log(`Your Number for Factorial is ${num1}`);
+console.log(`Your Factorial number is ${factorial(num1)}`);
