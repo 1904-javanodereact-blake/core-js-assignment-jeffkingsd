50ee1659af4c86367a0c6cc55bcdf1b8719d1b30
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-04-03 16:07:11 -0400

    Shapes is finished

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -22,35 +22,71 @@ Example for printShape("Diamond", 5, "*");
 */
 function printShape(shape, height, character) {
   let rowarr = [];
-  let colarr = [];
+  let rowout;
+  let spaces;
   switch (shape) {
     case 'square':
-      for(i = 0; i < height; i++){
+    rowout = character;
+      for(i = 0; i < height - 1; i++){
        rowarr.push(character);
-        for(j = 0; j < height; j++){
-         colarr.push(character);
-        }
-      } 
-      console.log(rowarr[colarr]);
+       rowout = rowout + '  ' + rowarr[i];
+      }
+      for (j = 0; j < height; j++)
+      {
+        console.log(rowout);
+      }
       break;
     case 'triangle':
-
-
-
+    rowout = character;
+    console.log(rowout);
+    for (i = 0; i < height -1 ; i++)
+    {
+        rowarr.push(character);
+        rowout = rowout + '  ' + rowarr[i];
+        console.log(rowout);
+    
+      }
+    
       break;
-    case 'Diamond':
+    case 'diamond':
+      for (i = 1; i <= height; i++)
+      {
+        rowout ='';
+        for (j= 1; j <= height - i; j++)
+        {
+          rowout += ' ';
+        }
 
+        for (g = 1; g <= 2*i-1; g++)
+        {
+          rowout += character;
+        }
+        
+        console.log(rowout)
+      }
 
-
+      for (i = height; i >= 0; i--){
+        rowout =' ';
+        for (j = height; j - 1 >= 0 + i; j--){
+          rowout += ' ';
+        }
+        for (g = 3; g <= 2*i-1; g++)
+        {
+          rowout+= character;
+        }
+        console.log(rowout);
+      }
       break;
     default:
+
+
       break;
   }
 }
 
 //Assuming a lot of things here
-let shape = 'square';
-let height = 15;
+let shape = 'diamond';
+let height = 10;
 const character = '&'
 
 printShape(shape, height, character);
