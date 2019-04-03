1843364edfe885868def6412bccb928e8c476684
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-04-03 19:14:26 -0400

    Object Prop is finished

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,20 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+    
+    let objectProp = {...someObj}
+    console.log('Object typeof and properties');
+    for(key in objectProp){
+        console.log(`Object Property = ${key} and value = ${objectProp[key]}`);
+    }
 }
+
+let chair = {
+    material: 'Alumnium',
+    weight: 40,
+    year: 2015,
+    usage: 'office',
+    isbroken: false,
+    hascushion: true
+};
+objectProperties(chair);
