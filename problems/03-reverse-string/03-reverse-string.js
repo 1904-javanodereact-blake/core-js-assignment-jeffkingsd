2982ad5963e2b969b8792d52c5d00a8871e04bf4
jeffkingsd <44207764+jeffkingsd@users.noreply.github.com>
2019-04-02 21:02:22 -0400

    Reversed String Finished

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,18 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+let StrSplit = someStr.split(''); //Splitting String by each character and turns it into an array
+let reverseStr = '';              //Holding the value and building the charcter back into string or Array????
+
+
+for(i = StrSplit.length - 1; i >= 0; i--)
+{
+ reverseStr = reverseStr + someStr[i];
 }
+return reverseStr;
+}
+
+
+let testStr = ('illimuniatiiswatchingus');
+console.log(`Normal String =  ${testStr}`);               //Normal String
+console.log(`Reverse String = ${reverseStr(testStr)}`);   //Reversed String
